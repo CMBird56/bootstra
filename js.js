a96ba8663982060f390e765c41ea547058dea252
CMBird56 <CMBird56@users.noreply.github.com>
2016-09-15 16:45:09 +0200

    diplomski animacije

---
+++ js.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("button").click(function(){
+    	$('body').prepend('<img src="123.jpg" />');
+        alert("Mwa!! Here is a kiss too sweetie!!");
+
+
+        $( "div" ).css( "background", "red" );
+    });
+
+
+
+
+
+});
+
